34816dcd61854e8597700acbc7804ffa8df0984d
Silene Tarquino Velasco <velascosilene@gmail.com>
2018-09-16 23:41:00 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # teste-python
+# teste-python
---
+++ scripts-python/teste01.py
@@ -0,0 +1,4 @@
+numero1 = input ('DIGITO O PRIMEIRO NUMERO!')
+numero2 = input('DIGITE O SEGUNDO NUMERO')
+soma = int(numero1) + int(numero2)
+print ( soma,'esse e o total.')
