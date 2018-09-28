d191f4320e47e9880a9b3c48260b9ff12bc82f2c
Silene Tarquino Velasco <velascosilene@gmail.com>
2018-09-28 00:34:53 -0300

    challenge timer and loop

--- temporizador.py
+++ temporizador.py
@@ -4,9 +4,8 @@ import webbrowser
 total_breaks = 3
 break_count = 0
 
-print ("This program started on" + time.ctime())
-while(break_count < total_breaks):
-    time.sleep(10)
-    webbrowser.open ("https://www.youtube.com/watch?v=KF322xXDffg&list=RDMMKF322xXDffg&start_radio=1")
+print ("This program started on "+time.ctime())
+while (break_count < total_breaks):
+    time.sleep(2*60*60)
+    webbrowser.open("http:/www,youtube.com/watch?v=HXcnDz_69J4")
     break_count = break_count + 1
-
