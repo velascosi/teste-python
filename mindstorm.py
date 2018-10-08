7493d323c453dcb80b6e271cc6777afcfde10da3
Silene Tarquino Velasco <velascosilene@gmail.com>
2018-10-08 09:32:47 -0300

    update of turtle run a circle and cube

--- mindstorm.py
+++ mindstorm.py
@@ -4,6 +4,10 @@ def draw_square():
     window.bgcolor("red")
 
     brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
@@ -12,6 +16,11 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
+
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
 
     window.exitonclick()
 
